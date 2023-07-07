20400f405ce7e8f1bf8730a834e70e2fd6a5cb04
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-07-07 20:55:03 +0530

    Time: 3 ms (51.25%), Space: 5.9 MB (46.9%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int reverse(int x) {
+
+    long res = 0;
+    
+    while(x != 0)
+    {
+        if (res > INT32_MAX / 10 || res < INT32_MIN / 10)
+                return 0;
+        res = res*10 + x%10;;
+        x = x/10;
+    }
+    
+    return res;
+    }
+};
