0685069cb7fd0b455ce49cf39db92a2a9e149aaf
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-07-13 10:51:24 +0530

    Time: 0 ms (100%), Space: 7.1 MB (86.66%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+    
+    int n = s.length();
+    
+     //phle poori string reverse karenge
+    
+     reverse(s.begin(), s.end());
+
+     int i = 0;
+     int l = 0, r = 0;
+
+     while(i < n)
+     {
+         while(i < n && s[i] != ' ')
+         {
+             s[r++] = s[i++];  // i ne r ko jo bhi string encounter hua use assign kar diya
+         }
+             //jaise hi space encounter kiya i ne tab l se leke r tak ke string reverse ho jayenge
+            if(l < r)  // l.......r   
+           {
+             reverse(s.begin() + l, s.begin() + r);
+            // ab l jo hai r ke pass aa jayega
+             s[r] = ' ';
+             r++;
+             l = r;
+           }
+        i++;
+     }
+           // ek extra space bach jayega last me isliye use remove kr denge
+         s = s.substr(0, r-1); // (star index, length)
+
+         return s;
+    }
+};
