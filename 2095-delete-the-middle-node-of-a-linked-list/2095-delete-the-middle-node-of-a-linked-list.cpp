0bdb8905e2c1d7827259166244e9a090e6eb83c2
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-07-18 00:15:56 +0530

    Time: 1034 ms (96.88%), Space: 294.8 MB (41.97%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+         ListNode* pre = NULL;
+         ListNode* slow = head;
+         ListNode* fast = head;
+         
+         if(head->next == NULL)
+             return NULL;
+        
+         while(fast != NULL && fast->next != NULL)
+         { 
+            pre = slow; 
+            slow = slow->next;
+            fast = fast->next->next;
+
+         }
+         pre->next = slow->next;
+         delete(slow);
+        
+         return head;
+    }
+};
