c9c8946139cb3f8e4918980cc0f463461a360ab0
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-07-21 00:41:01 +0530

    Time: 5 ms (39.18%), Space: 6.5 MB (85.45%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -5,11 +5,9 @@ public:
         int count = 0;
         int flag = 0; 
         for(int i = n-1; i>=0; i--)
-        {
-            
+        {    
 /*agr hme blank space mil ra h and flag 1 hai iska mtlb hme words mil chuke hain to ab break krke loop ke bahar aa jayenge*/
 /*or fir count return kr denge*/
-            
             if(s[i]==' ' && flag == 1)
                 break;
             if(s[i] != ' ')
