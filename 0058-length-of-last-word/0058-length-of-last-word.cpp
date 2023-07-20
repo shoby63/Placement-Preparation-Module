d2048eaf536066b75a2536009514e0b1afc19e31
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-07-21 00:40:19 +0530

    Time: 5 ms (39.18%), Space: 6.5 MB (39.07%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n = s.size();
+        int count = 0;
+        int flag = 0; 
+        for(int i = n-1; i>=0; i--)
+        {
+            
+/*agr hme blank space mil ra h and flag 1 hai iska mtlb hme words mil chuke hain to ab break krke loop ke bahar aa jayenge*/
+/*or fir count return kr denge*/
+            
+            if(s[i]==' ' && flag == 1)
+                break;
+            if(s[i] != ' ')
+            {
+                flag = 1;
+                count++;
+            }
+        }
+        return count;
+    }
+};
