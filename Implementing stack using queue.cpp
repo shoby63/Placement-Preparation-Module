6033f3cbc1a0a0db330052c846516fd9462e1213
Shivangi2430 <103505371+Shivangi2430@users.noreply.github.com>
2023-07-24 22:19:53 +0530

    Create Implementing stack using queue.cpp

---
+++ Implementing stack using queue.cpp
@@ -0,0 +1,43 @@
+// TC = O(n)
+// SC = O(n)
+
+class MyStack {
+public:
+    queue<int>que;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        que.push(x);
+        for(int i = 0; i < que.size() - 1; i++) //iteration till one size lesser
+        {
+            que.push(que.front());
+                que.pop();
+        }
+        
+    }
+    
+    int pop() {
+       int x = que.front();
+        que.pop();
+        return x;
+    }
+    
+    int top() {
+        return que.front();
+    }
+    
+    bool empty() {
+        return que.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
